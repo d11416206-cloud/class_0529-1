58ebb757e2ce59000a9a49880882eaeb7b42c552
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-05-29 10:39:04 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Person
+{
+    public virtual string GetName()
+    {
+        return "Person";
+    }
+    
+}
+
+public class Student : Person
+{
+    public sealed override string GetName()
+    {
+        return "Student";
+    }
+}
+
+public class Teacher : Student
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
